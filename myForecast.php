a241be695d5d094908f46eb2870a062751b9c8d0
codevelop <codevelopx@gmail.com>
2019-06-11 21:59:41 +0200

    removed bugs and added comments

--- myForecast.php
+++ myForecast.php
@@ -1,3 +1,8 @@
+<!-- obróbka danych w celu wygenerowania dziennej prognozy pogody na podstawie danych uzyskanych z api z 5-cio dniowej 3 godzinnej prognozy
+m.in ustalenie wschodu i zachodu słońca w celu określenia godzin dnia i nocy, by wybrać max i min temperaturę w każdym dniu prognozy
+wyliczenie średnich wartości wiatru i ciśnienia dla każdego dnia
+zapis uzyskanych danych do tablicy obiektów -->
+
 <?php
 
 class MyForecast
@@ -63,33 +68,20 @@ class MyForecast
 
     function endDayHour()
     {
+
         foreach ($this->dataTime as $key => $value) {
-            // echo '<br>';
-            // echo 'value: ', $value;
-            // echo '<br>';
-            // echo 'sunRise: ', $this->sunRise;
-            // echo '<br>';
-            // echo 'sunSet: ', $this->sunSet;
 
             if ($this->sunSet >= $this->dataTime[7]) {
                 $this->endDayHour = $this->dataTime[7];
+                $this->endDayIndex = 7;
+                return;
             } else {
-
                 if ($this->sunSet <= $value) {
-                    // echo '<br>Środek pętli warunek spełniony: <br>';
-                    // echo '<br>';
-                    // echo '$key: ', $key, 'value: ',  $value;
-                    // echo '<br>';
-                    // echo 'sunrise ', $this->sunRise;
-
                     if ($value - $this->sunSet <= 90) {
-                        // echo '<br> $value-$this->sunSet', $value - $this->sunSet;
-                        // echo '<br>Początek dnia:', $this->dataTime[$key];
                         $this->endDayIndex = $key;
                         return;
                     } else {
-                        // echo '<br> $value-$this->sunSet', $value - $this->sunSet;
-                        // echo '<br>Koniec dnia:', $this->dataTime[$key - 1];
+
                         $this->endDayIndex = $key - 1;
                         return;
                     }
@@ -109,31 +101,16 @@ class MyForecast
         $maxTempD = array();
         $minTempN = array();
 
-        // echo '<br>';
-        // echo 'startDay: ', $this->dataTime[$this->startDayIndex];
-        // echo '<br>';
-        // echo 'endDay: ', $this->dataTime[$this->endDayIndex];
 
         foreach ($this->data['list'] as $key => $value) {
             if (!$key) {
                 $currentData = date('Y-m-d', $value['dt']);
                 $prevData = $currentData;
             }
-            // echo '<br>', $value['dt'];
-            // echo '<br>', date('Y-m-d', $value['dt']);
-
-
 
 
             if ($currentData !== (date('Y-m-d', $value['dt']))) {
-                // echo '<br>Zmiana daty - działamy:';
 
-
-                // echo '<br>';
-
-                // echo '<br>', $this->dataTime[$this->startDayIndex];
-                // echo '===';
-                // echo intval(date('H', ($value['dt'] - date("Z")))) * 60;
 
                 if (($this->dataTime[$this->startDayIndex] <=  intval(date('H', ($value['dt'] - date("Z")))) * 60) && ($this->dataTime[$this->endDayIndex] >=  intval(date('H', ($value['dt'] - date("Z")))) * 60)) {
                     // echo 'Zapamiętujemy temperatury';
@@ -145,18 +122,12 @@ class MyForecast
                     array_push($tempNight, $value['main']['temp']);
                 }
 
-                // echo '<br>$prevData: ', $prevData;
-                // echo '<br> date(" Y - m - d ", $value[" dt "]): ', date('Y-m-d', $value['dt']);
-
-
 
                 if ($prevData !== date('Y-m-d', $value['dt'])) {
                     // echo '<br>Kolejny dzień:';
 
                     if ($tempDay) {
                         array_push($tempD, $tempDay);
-                        echo '<br><br>tempDay:';
-                        print_r($tempDay);
                         array_splice($tempDay, 0);
                     }
                     if ($tempNight) {
@@ -164,7 +135,6 @@ class MyForecast
                         array_splice($tempNight, 0);
                     }
 
-                    // echo 'data do zapisania: ', date('Y-m-d', $value['dt']);
                     array_push($this->dateMyForecast, date('Y-m-d', $value['dt']));
 
                     $prevData = date('Y-m-d', $value['dt']);
@@ -192,39 +162,23 @@ class MyForecast
             array_push($minTempN, min($tempN[$i]));
         }
 
-        echo '---------------<br>';
-        echo '--------tempD-------<br>';
-        print_r($tempD);
-        echo '---------------<br>';
-        echo '--------counttempD-------<br>';
-        echo count($tempD);
-
-        for ($i = 0; $i < count($tempD); $i++) {            //-1
+        for ($i = 0; $i < count($tempD); $i++) {
 
             $this->myForecast[$i] = new stdClass();
 
             $this->myForecast[$i]->date = $this->dateMyForecast[$i];
             $this->myForecast[$i]->maxTemp = $maxTempD[$i];
             $this->myForecast[$i]->minTemp = $minTempN[$i];
-            $this->myForecast[$i]->wind = $this->windMyForecast[$i];
-            $this->myForecast[$i]->pressure = $this->pressureMyForecast[$i];
+            $this->myForecast[$i]->wind = round($this->windMyForecast[$i], 2);
+            $this->myForecast[$i]->pressure = round($this->pressureMyForecast[$i], 2);
         }
-
-
-        echo '---------------<br>';
-        echo '---------------<br>';
-
-        var_dump($this->myForecast);
-
-        echo '---------------<br>';
-        echo '---------------<br>';
     }
 
 
     function showMyForecast()
     {
         echo '<div class="myForecastTable">';
-        echo '<table border="1">';
+        echo '<table>';
 
         foreach ($this->myForecast as $key => $value) {
             echo '<tr>';
